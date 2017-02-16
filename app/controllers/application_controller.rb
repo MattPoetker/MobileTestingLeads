643657dcab7855db7d9d25fe6217a3bbf9022034
Matthew Poetker <matt.pworks@gmail.com>
2017-02-15 23:26:34 -0600

    Added POST request feature.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
+  skip_before_filter :verify_authenticity_token
+
+  def test
+  	@url = params[:field]
+  	@response = Net::HTTP.get_response("https://searchconsole.googleapis.com/v1/urlTestingTools/mobileFriendlyTest:run?url=#{@url}&requestScreenshot=true&key=AIzaSyD8Abp3X5U4bZBFuyPoIUd3LfZOLbVeKX8")
+  	
+  end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,16 @@
 class WelcomeController < ApplicationController
 	require 'http'
+	require "net/http"
+	require "uri"
 
 	def index
+		
+	end
+	def show
+		@url = params[:field]
+
+		uri = URI('https://searchconsole.googleapis.com/v1/urlTestingTools/mobileFriendlyTest:run')
+		@response = Net::HTTP.post_form(uri, 'url' => @url, 'requestScreenshot' => false, 'key' => 'AIzaSyD8Abp3X5U4bZBFuyPoIUd3LfZOLbVeKX8')
 		
 	end
 end
